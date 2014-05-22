3cfd72a3394c6194a680378d6c275474b819dc73
kouta <dev2@exo-group.com>
2014-05-22 15:48:51 +0400

    Post request supported.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var http = require("http");
 var url = require('url');
+var qs = require('querystring');
+
+
 
 http.createServer(function(request, response) {
+    if (request.method == 'POST') {
+        var body = "";
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            var post = qs.parse(body)
+            console.log(post);
+        });
+        response.end();
+    }
     var get_data = url.parse(request.url, true);
     if (get_data.query['hub.verify_token'] != undefined && get_data.query['hub.verify_token'] == 'instagramPrintVerify') {
         console.log(get_data.query);
@@ -11,4 +26,5 @@ http.createServer(function(request, response) {
     } else {
         response.end();
     }
+
 }).listen(8885);
