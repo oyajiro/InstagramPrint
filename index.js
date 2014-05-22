b8a11b25268b6465af9c18e61831269b820faaa7
kouta <dev2@exo-group.com>
2014-05-22 15:13:41 +0400

    Correct verify condition.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var http = require("http");
 var url = require('url');
 
 http.createServer(function(request, response) {
-    if (request.method == 'GET') {
-        var get_data = url.parse(request.url, true);
+    var get_data = url.parse(request.url, true);
+    if (get_data.query['hub.verify_token'] != undefined && get_data.query['hub.verify_token'] == 'instagramPrintVerify') {
         console.log(get_data.query);
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(get_data.query['hub.challenge']);
+        response.end();
+    } else {
         response.end();
     }
 }).listen(8885);
