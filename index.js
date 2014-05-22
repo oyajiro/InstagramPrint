bba3a6703fd770f34d1cee833115c586f22271f6
kouta <dev2@exo-group.com>
2014-05-22 13:19:06 +0400

    Modify to instagramm chalenge.

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ http.createServer(function(request, response) {
     if (request.method == 'GET') {
         var get_data = url.parse(request.url, true);
         response.writeHead(200, {"Content-Type": "text/plain"});
-        //console.log(get_data.query['a']);
-        response.write(get_data.query['a']);
+        response.write(get_data.query['hub.challenge']);
         response.end();
     }
 }).listen(8885);
